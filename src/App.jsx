c88fc0e8bdc4f4e410becfd29d15083cd502caf9
geniuspulse <geniuspulse22@gmail.com>
2026-06-19 11:55:15 +0200

    feat: add /pricing route

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ import LeadForms from '@/pages/LeadForms';
 import DesignerPortal from '@/pages/DesignerPortal';
 import Referrals from '@/pages/Referrals';
 import About from '@/pages/About';
+import Pricing from '@/pages/Pricing';
 import Contact from '@/pages/Contact';
 import PrivacyPolicy from '@/pages/PrivacyPolicy';
 import Terms from '@/pages/Terms';
@@ -121,6 +122,7 @@ const AuthenticatedApp = () => {
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/onboarding" element={<Onboarding />} />
       <Route path="/about" element={<About />} />
+            <Route path="/pricing" element={<Pricing />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route path="/terms" element={<Terms />} />
